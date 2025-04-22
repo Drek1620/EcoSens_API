e507f39f7363ed822ebfe13a185cdbcadfdc49fe
matrixfeniko <matrixfeniko33@gmail.com>
2025-04-21 21:28:17 -0600

    Reapply "controller corregido (no pude eliminar las otras clases pq no me daba lo aopcion borralas porfa alan :p)"
    This reverts commit b22906ff7326e28ef539fa8bca56a49c33dc2a84.

--- Controllers/DelController.cs
+++ Controllers/DelController.cs
@@ -7,60 +7,9 @@ using System.Text;
 using System.Security.Claims;
 using System.IdentityModel.Tokens.Jwt;
 using Microsoft.EntityFrameworkCore;
-using MongoDB.Driver;
 using Microsoft.EntityFrameworkCore.Internal;
 
 namespace EcoSens_API.Controllers
 { 
-    
-    [Route("api/edit")]
-        [ApiController]
-    public class DelController : ControllerBase
-    {
-       
 
-        private readonly AppDbContext _context;
-        private readonly IConfiguration _config;
-        private readonly mongoDbService _mongoDbService;
-
-        public DelController(AppDbContext context, IConfiguration config, mongoDbService mongoDbService)
-        {
-            _context = context;
-            _config = config;
-            _mongoDbService = mongoDbService;
-
-        }
-
-        [HttpDelete("Delcont/{id}")]
-
-        public IActionResult editarcontenedor(int id)
-        {
-            try
-            {
-                var contenedor = _context.Contenedores.FirstOrDefault(contenedor => contenedor.Id == id);
-
-
-                _context.Remove(contenedor);
-                _context.SaveChanges();
-
-
-                if (contenedor == null)
-                {
-                    return NotFound("Contenedor no encontrado.");
-                }
-
-                return Ok(contenedor);
-
-            }
-            catch (Exception ex)
-            {
-                return BadRequest("Ha ocurrido una excepcion");
-            }
-
-           
-        }
-
-
-      
-    }
 }
